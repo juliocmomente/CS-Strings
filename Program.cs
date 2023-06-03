b5a2451226f0b4fc30136fe34a9bacabfc0e822d
Julio Cesar Momente <juliocesar.momente@gmail.com>
2023-06-02 21:25:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Olá, mundo!");
+Console.WriteLine("Qual o seu nome?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Olá, {name}, Hoje é  {currentDate:d}, agora são {currentDate:t}!");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
